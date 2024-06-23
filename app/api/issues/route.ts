ef4462820d04eff7ed1900bf50b89474f2bfda0e
Shorno <fb.shorno@gmail.com>
2024-06-24 02:46:26 +0600

    client side validation

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,16 +1,11 @@
 import {NextRequest, NextResponse} from "next/server";
-import {z} from "zod"
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-    title: z.string().min(5,"Title is required").max(255),
-    description: z.string().min(10, "Description is required")
-})
+import {validationSchemas} from "@/app/validationSchemas";
 
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    const validation = createIssueSchema.safeParse(body);
+    const validation = validationSchemas.safeParse(body);
 
     if (!validation.success)
         return NextResponse.json(validation.error.format(), {status: 404})
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,63 +1,60 @@
 "use client"
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import {Button, Callout, TextField} from "@radix-ui/themes";
+import {Button, Callout, Text, TextField} from "@radix-ui/themes";
 import {useForm, Controller} from "react-hook-form";
 import axios from "axios";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {createIssueSchema} from "@/app/validationSchemas";
+import {z} from "zod"
 
+type IssueForm = z.infer<typeof createIssueSchema>
 
-interface IssueForm {
-    title: string,
-    description: string
-}
-
-
-function InfoCircledIcon() {
-    return null;
-}
 
 export default function NewIssuePage() {
     const router = useRouter();
     const [error, setError] = useState("")
-    const {register, control, handleSubmit} = useForm<IssueForm>()
+    const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+    })
     return (
         <>
             <div className={"max-w-xl"}>
 
                 {
                     error && (
-                    <Callout.Root color={"red"} className={"mb-5"}>
-                        <Callout.Icon>
-                            <InfoCircledIcon />
-                        </Callout.Icon>
-                        <Callout.Text>
-                            {error}
-                        </Callout.Text>
-                    </Callout.Root>
+                        <Callout.Root color={"red"} className={"mb-5"}>
+                            <Callout.Text>
+                                {error}
+                            </Callout.Text>
+                        </Callout.Root>
                     )
                 }
 
-            <form className={"space-y-3"} onSubmit={handleSubmit(async (data) => {
-               try {
-                   await axios.post("/api/issues", data);
-                   router.push("/issues")
-               }catch (error){
-                   setError("Unexpected error occurred")
-               }
-            })}>
-                <TextField.Root placeholder="Title" {...register("title")}>
-                    <TextField.Slot>
-                    </TextField.Slot>
-                </TextField.Root>
-                <Controller
-                    name={"description"}
-                    control={control}
-                    render={({field}) => <SimpleMDE placeholder="Description" {...field}/>
-                    }/>
-                <Button>Submit New Issue</Button>
-            </form>
+                <form className={"space-y-3"} onSubmit={handleSubmit(async (data) => {
+                    try {
+                        await axios.post("/api/issues", data);
+                        router.push("/issues")
+                    } catch (error) {
+                        setError("Unexpected error occurred")
+                    }
+                })}>
+                    <TextField.Root placeholder="Title" {...register("title")}>
+                        <TextField.Slot>
+                        </TextField.Slot>
+                    </TextField.Root>
+                    {errors.title && <Text color={"red"} as={"p"}>{errors.title.message}</Text>}
+                    <Controller
+                        name={"description"}
+                        control={control}
+                        render={({field}) => <SimpleMDE placeholder="Description" {...field}/>
+                        }/>
+                    {errors.description && <Text color={"red"} as={"p"}>{errors.description.message}</Text>}
+
+                    <Button>Submit New Issue</Button>
+                </form>
             </div>
         </>
     )

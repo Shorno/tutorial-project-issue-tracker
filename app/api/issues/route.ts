7971dd0ba0301dfc230462e21c5ecff083f4292b
Shorno <fb.shorno@gmail.com>
2024-06-24 01:56:01 +0600

    error handling

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import {z} from "zod"
 import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
-    title: z.string().min(5).max(255),
-    description: z.string().min(10)
+    title: z.string().min(5,"Title is required").max(255),
+    description: z.string().min(10, "Description is required")
 })
 
 
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
 
     if (!validation.success)
-        return NextResponse.json(validation.error.errors, {status: 404})
+        return NextResponse.json(validation.error.format(), {status: 404})
 
 
     const newIssue = await prisma.issue.create({

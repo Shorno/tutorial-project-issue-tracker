10438b3e08370d162c7076f7e105febe89de25c7
Shorno <fb.shorno@gmail.com>
2024-06-24 01:30:43 +0600

    handle from submission

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
             <main className={"p-5"}>
                 {children}
             </main>
-            <ThemePanel/>
         </Theme>
         </body>
         </html>

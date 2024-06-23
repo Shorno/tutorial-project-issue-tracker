b03a0721df8e6be55e3baa2c56a7ca8ab6f6df51
Shorno <fb.shorno@gmail.com>
2024-06-23 23:15:58 +0600

    radix ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import '@radix-ui/themes/styles.css';
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
+import "./theme-config.css"
 import "./globals.css";
 import Navbar from "@/app/Navbar";
-import {Theme} from "@radix-ui/themes";
+import {Theme, ThemePanel} from "@radix-ui/themes";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -19,12 +24,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>
-        <Theme>
+        <body className={inter.variable}>
+        <Theme accentColor="iris" grayColor="sage">
             <Navbar/>
             <main className={"p-5"}>
                 {children}
             </main>
+            <ThemePanel/>
         </Theme>
         </body>
         </html>

bf1d24a0e75005bf03842e5e314ac3d0785c0e86
Shorno <fb.shorno@gmail.com>
2024-06-26 01:17:00 +0600

    handle error

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,8 +2,21 @@
 import {Button, AlertDialog, Flex} from "@radix-ui/themes";
 import axios from "axios";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
 
 export default function DeleteIssueButton({issueId}: { issueId: number }) {
+
+    const [error, setError] = useState(false)
+
+    const deleteIssue = async () => {
+        try {
+            await axios.delete(`/api/issues/` + issueId);
+            router.push("/issues");
+            router.refresh();
+        } catch (error) {
+            setError(true)
+        }
+    }
 
     const router = useRouter()
     return (
@@ -27,16 +40,26 @@ export default function DeleteIssueButton({issueId}: { issueId: number }) {
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
                             <Button
-                                onClick={async () => {
-                                    await axios.delete(`/api/issues/` + issueId);
-                                    router.push("/issues");
-                                    router.refresh();
-                                }}
+                                onClick={deleteIssue}
                                 variant="solid" color="red"
                             >
                                 Delete Issue
                             </Button>
                         </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content maxWidth={"450px"}>
+                    <AlertDialog.Title>Error </AlertDialog.Title>
+                    <AlertDialog.Description size={"2"}>
+                        Unexpected error occurred deleting the issue.
+                    </AlertDialog.Description>
+                    <Flex justify={"end"}>
+                        <Button color={"gray"} variant={"soft"} mt={"2"}
+                                onClick={() => setError(false)}
+                        >Ok</Button>
                     </Flex>
                 </AlertDialog.Content>
             </AlertDialog.Root>

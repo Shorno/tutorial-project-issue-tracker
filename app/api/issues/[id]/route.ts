09c7ebccf3fea0b67de37fbc16570025e16eac2c
Shorno <fb.shorno@gmail.com>
2024-06-26 00:46:59 +0600

    issue delete API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 
 
 export async function PATCH(request: NextRequest,
-                                    {params}: { params: { id: string } }) {
+                            {params}: { params: { id: string } }) {
 
 
     const body = await request.json();
@@ -32,5 +32,26 @@ export async function PATCH(request: NextRequest,
     })
 
     return NextResponse.json(updatedIssue);
+}
+
+
+export async function DELETE(request: NextRequest,
+                             {params}: { params: { id: string } }) {
+
+    const issue = await prisma.issue.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+
+    if (!issue) {
+        return NextResponse.json("Invalid issue", {status: 404});
+    }
+
+    await prisma.issue.delete({
+        where: {id: issue.id}
+    })
+
+    return NextResponse.json({message: "Issue deleted successfully"})
 
 }
+
+
